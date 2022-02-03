ec425bff588df1d00342a865aa9a5a84eeebed87
hoseindig <hosein_dig@yahoo.com>
2022-02-03 16:47:45 +0330

    news

--- src/component/cardNewMainPage.jsx
+++ src/component/cardNewMainPage.jsx
@@ -4,45 +4,51 @@ const CardNewsMainPAge = () => {
   const news = [
     {
       id: 1,
-      text: "اخبار",
+      text: "بیمارستان ولایت مشهد مقدس افتتاح شد ",
     },
     {
       id: 2,
-      text: "اخبار ",
+      text: "همنوایی کامل کمیته علمی و ستاد ملی مقابله با کرونا ",
+
     },
     {
       id: 3,
-      text: "اخبار ",
+      text: "اعلام آماده باش به مراکز بهداشتی کشور با ورود به موج ششم کرونا ",
+
     },
     {
       id: 5,
-      text: "اخبار ",
+      text: "مدیرعامل سازمان انتقال خون ایران منصوب شد ",
     },
 
     {
       id: 6,
-      text: "اخبار ",
+      text: "تزریق ۶۲۱ هزار دُز واکسن کرونا در شبانه روز گذشته ",
     },
     {
       id: 7,
-      text: "اخبار",
+      text: "منابع پیش بینی شده نظام سلامت را به دستگاه های دیگر ندهید",
     },
     {
       id: 8,
-      text: "اخبار ",
+      text: " روند بروز سرطان در ایران افزایشی است ",
     },
     {
       id: 9,
-      text: "اخبار ",
+      text: "هیچ مرزی بین ایران و سوریه وجود ندارد ",
     },
     {
       id: 10,
-      text: "اخبار ",
+      text: "انتقال موفقیت آمیز قلب قهرمان 1.5 ساله مشهدی به تهران ",
     },
-    {
+     {
       id: 11,
-      text: "اخبار ",
+      text: "راهکارهایی برای مدیریت نگرانی های غیرمفید در دوران همه گیری کرونا ",
     },
+    // {
+    //   id: 12,
+    //   text: "اخبار ",
+    // },
   ];
   return (
     <Card border="light" className="card-news">
@@ -56,7 +62,7 @@ const CardNewsMainPAge = () => {
               return (
                 <ListGroup.Item as="li" key={n.id}>
                   <i className="fa fa-circle" style={{ fontSize: "8px" }}></i>{" "}
-                  {n.text}
+                  <span>{n.text}</span>
                   <i className="fa fa-bookmark-o" aria-hidden="true"></i>
                 </ListGroup.Item>
               );
