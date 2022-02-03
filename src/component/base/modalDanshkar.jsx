902d3aa1bcd46f9034fa7de8bfe71276d72ab39c
hoseindig <hosein_dig@yahoo.com>
2022-02-03 13:17:28 +0330

    change modale grid to MUI table

--- src/component/base/modalDanshkar.jsx
+++ src/component/base/modalDanshkar.jsx
@@ -1,7 +1,42 @@
-import { Button, Modal, Table, Row, Col } from "react-bootstrap";
+import { Button, Modal, Row, Col } from "react-bootstrap";
 import SearchInput from "../search";
-const ModalDanshkar = ({ show, toggleModal }) => {
+import Table from './table'
+
+const ModalDanshkar = ({ show, toggleModal, handleChange, handleAddUserSelected, name }) => {
   console.log("showModal", show);
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'نام', width: 130 },
+    { field: 'lastName', headerName: 'نام فامیل', width: 130 },
+    {
+      field: 'age',
+      headerName: 'سن',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'fullName',
+      headerName: 'نام و نام خانوادگی',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.getValue(params.id, 'firstName') || ''} ${params.getValue(params.id, 'lastName') || ''
+        }`,
+    },
+  ];
+  const rows = [
+    { id: 1, lastName: 'پیوندی  ', firstName: 'حسین', age: 35 },
+    { id: 2, lastName: 'تابش', firstName: 'افسون ', age: 42 },
+    { id: 3, lastName: 'تبریزی', firstName: 'اهورا ', age: 45 },
+    { id: 4, lastName: 'طالقانی ', firstName: 'ايرج ', age: 16 },
+    { id: 5, lastName: 'صفوی', firstName: 'بابک ', age: null },
+    { id: 6, lastName: 'شهیدی', firstName: "پایا", age: 150 },
+    { id: 7, lastName: 'شجاعی', firstName: 'برديا ', age: 44 },
+    { id: 8, lastName: 'سراج ', firstName: 'برزويه ', age: 36 },
+    { id: 9, lastName: 'توفیقی', firstName: 'برمک ', age: 65 },
+    { id: 9, lastName: ' جلالی', firstName: null, age: 65 },
+];
   return (
     <>
       <Modal
@@ -21,43 +56,40 @@ const ModalDanshkar = ({ show, toggleModal }) => {
                 <SearchInput label="جستجوی نام" />
               </Col>
               <Col md={4}>
-                <SearchInput  label="جستجوی نام خانوادگی"/>
+                <SearchInput label="جستجوی نام خانوادگی" />
               </Col>
               <Col md={4}>
-                <SearchInput  label="جستجوی کد پرسنلی"/>
+                <SearchInput label="جستجوی کد پرسنلی" />
               </Col>
             </Row>
             <Row>
               <Col>
-                <Table striped bordered hover>
+                <Table rows={rows} columns={columns} handleChange={handleChange} name={name} multiSelect={false} />
+                {/* <Table striped bordered hover>
                   <thead>
                     <tr>
                       <th>#</th>
-                      <th>First Name</th>
-                      <th>Last Name</th>
-                      <th>Username</th>
+                      <th>نام</th>
+                      <th>نام خانوادگی</th>
+                      <th>نام کاربری</th>
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
                       <td>1</td>
-                      <td>Mark</td>
-                      <td>Otto</td>
+                      <td>حسین</td>
+                      <td>حسنی</td>
                       <td>@mdo</td>
                     </tr>
                     <tr>
                       <td>2</td>
-                      <td>Jacob</td>
-                      <td>Thornton</td>
+                      <td>رضا</td>
+                      <td>جعفری</td>
                       <td>@fat</td>
                     </tr>
-                    <tr>
-                      <td>3</td>
-                      <td colSpan={2}>Larry the Bird</td>
-                      <td>@twitter</td>
-                    </tr>
+                    
                   </tbody>
-                </Table>
+                </Table> */}
               </Col>
             </Row>
           </div>
@@ -66,7 +98,7 @@ const ModalDanshkar = ({ show, toggleModal }) => {
           <Button variant="secondary" onClick={toggleModal}>
             خروج
           </Button>
-          <Button variant="primary">تایید</Button>
+          <Button variant="primary" onClick={handleAddUserSelected}>تایید</Button>
         </Modal.Footer>
       </Modal>
     </>
